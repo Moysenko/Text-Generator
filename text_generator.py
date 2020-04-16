0b9c1f02a6b28a8e6c6f377e32d52a21311b22d6
moysenko <moysenkom@gmail.com>
2020-04-16 19:57:54 +0500

    codestyle fixed, minor bugs with brackets fixed

--- text_generator.py
+++ text_generator.py
@@ -16,29 +16,30 @@ class generator:
         self.reset()
 
     def _is_valid_token(self, token):
-        if token in punctuation.close_brackets and\
+        if token in punctuation.CLOSE_BRACKETS and\
                 (not self.stack or punctuation.get_punctuation_pair(self.stack[-1]) != token):
             return False
-        if self.text and (self.text[-1] in punctuation.close_brackets or
-                          (self.text[-1] + token) in punctuation.valid_punctuation_pairs):
+        if self.text and (self.text[-1] in punctuation.CLOSE_BRACKETS or
+                          (self.text[-1] + token) in punctuation.VALID_PUNCTUATION_PAIRS):
             return True
         if (not self.text or self.text[-1] in string.punctuation) and token in string.punctuation:
             return False
         return True
 
-    def _modifyed_token(self, token):
+    def _get_modifyed_token(self, token):
         if token == '-':
             return ' ' + token
-        if (not token.isalpha() and token not in punctuation.open_brackets) or\
-                (self.text and self.text[-1] in punctuation.open_brackets):
+        if (not token.isalpha() and token not in punctuation.OPEN_BRACKETS) or\
+                (self.text and self.text[-1] in punctuation.OPEN_BRACKETS):
             return token
         if not self.text:
             return token.capitalize()
-        if self.text[-1] in punctuation.sentence_ending_punctuation:
+        if self.text[-1] in punctuation.SENTENCE_ENDING_PUNCTUATION:
             return ' ' + token.capitalize()
         return ' ' + token
 
     def _get_random_token(self, key):
+        self._make_valid_last_tokens_id()
         random_number = random.random()
         for token_id, chance in self.probability[key].items():
             if random_number < chance:
@@ -59,33 +60,51 @@ class generator:
                 print("ERROR!!!", self.text, self.probability[key])
                 sys.exit(228)
 
-    def _get_valid_last_tokens_id(self):
+    def _make_valid_last_tokens_id(self):
         while (len(self.last_tokens_id) > self.depth) or (tuple(self.last_tokens_id) not in self.probability) or\
                 not list(filter(lambda token_id: self._is_valid_token(self.id_to_word[token_id]),
                                 self.probability[tuple(self.last_tokens_id)].keys())):
             self.last_tokens_id.popleft()
 
     def _update_punctuation_stack(self, token):
-        if token in punctuation.paired_punctuation:
+        if token in punctuation.PAIRED_PUNCTUATION:
             if self.stack and self.stack[-1] == punctuation.get_punctuation_pair(token):
                 self.stack.pop()
             else:
                 self.stack.append(token)
 
-    def add_text(self, tokens_amount):
-        for step in range(tokens_amount):
-            self._get_valid_last_tokens_id()
+    def _get_generating_step(self, sentence_ending):
+        is_step_skipped = False
+        if sentence_ending:
+            if not self.stack:
+                token = sentence_ending
+                sentence_ending = None
+            else:
+                token = punctuation.get_punctuation_pair(self.stack[-1])
+        else:
             key = () if random.random() < self.uniform_proba else tuple(self.last_tokens_id)
             token = self._get_random_valid_token(key)
 
-            if token in punctuation.sentence_ending_punctuation and self.stack:
-                token = punctuation.get_punctuation_pair(self.stack[-1])
-                self.stack.pop()
+            if token in punctuation.SENTENCE_ENDING_PUNCTUATION and self.stack:
+                sentence_ending = token
+                is_step_skipped = True
 
-            self.text += self._modifyed_token(token)
+        return token, is_step_skipped, sentence_ending
+
+    def add_text(self, tokens_amount):
+        sentence_ending = None
+        tokens_generated = 0
+        while tokens_generated < tokens_amount:
+            token, is_step_skipped, sentence_ending = self._get_generating_step(sentence_ending)
+
+            if is_step_skipped:
+                continue
+
+            self.text += self._get_modifyed_token(token)
             self.last_tokens_id.append(self.word_to_id[token])
 
             self._update_punctuation_stack(token)
+            tokens_generated += 1
 
     def reset(self):
         self.last_tokens_id = collections.deque()
@@ -106,7 +125,7 @@ class generator:
         self.last_tokens_id = collections.deque(tokens_id)
 
     def get_probability(self, amount=10):
-        self._get_valid_last_tokens_id()
+        self._make_valid_last_tokens_id()
         possible_tokens_id = list(self.probability[tuple(self.last_tokens_id)].items())
         possible_tokens = [(self.id_to_word[token_id], probability) for token_id, probability in possible_tokens_id]
         print(tuple(self.id_to_word[token_id] for token_id in self.last_tokens_id))
